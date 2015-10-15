89935c0f8b9a150a8430bd6afccb6914c423cdfa
Paul Dolder <paul.dolder@cefas.co.uk>
2015-10-15 23:04:43 +0100

    Update MP_3f_CFP_MSY_HCR.R

--- R/MP_3f_CFP_MSY_HCR.R
+++ R/MP_3f_CFP_MSY_HCR.R
@@ -140,12 +140,11 @@ CFPMSYHCR <- function(stocks, advice, advice.ctrl, year, stknm,...){
       # Here we add the new calculation of Ftg dependent on Yrtg
       # When assyrnumb+1 >= Yrtg, Ftg=Ftg
       # When assyrnumb+1 < Yrtg, F is the linear difference between Fsq and Ftg
-      Yrtg<-as.numeric(Yrtg)
-      yr.now<-as.numeric(assyrnumb)
-      Ftg2<-ifelse(yr.now+1>=Yrtg,Ftg,((1-(1/(Yrtg-yr.now+1)))*fsq)+((1/(Yrtg-yr.now+1))*as.numeric(Ftg)))
+      
+      Ftg2<-ifelse(assyrnumb+1>=Yrtg,Ftg,((1-(1/(Yrtg-assyrnumb+1)))*fsq)+((1/(Yrtg-assyrnumb+1))*as.numeric(Ftg)))
       print(Ftg2)
       Ftg2<-min(max(fsq,as.numeric(Ftg)),Ftg2) # take the min or fsq or the new target, but ensuring fsq is not below Ftg
-      Ftg2<-as.character(Ftg2)
+      
           fwd.ctrl2 <- fwdControl(data.frame(year = c(assyrnumb+1, assyrnumb+1),  val = c(Ftg2,NA), quantity = c( 'f', Cadv),
                                            min = c(NA, TACnow[i]*Clo), max  = c(NA, TACnow[i]*Cup)))
         
