dd3ec8f716d3f984f9e55a973f5bb3d3e330d830
Paul Dolder <paul.dolder@cefas.co.uk>
2015-10-27 18:38:10 +0200

    Update OM_2a_Effort_Dynamics_SMFB_LO_exem.R

--- R/OM_2a_Effort_Dynamics_SMFB_LO_exem.R
+++ R/OM_2a_Effort_Dynamics_SMFB_LO_exem.R
@@ -297,8 +297,9 @@ SMFB_lo <- function(fleets, biols, covars, advice, fleets.ctrl, advice.ctrl, fln
                                         ifelse((diff - fleets.ctrl[[flnm]]$LandObl_minimis_p[,yr] - 1) < fleets.ctrl[[flnm]]$LandObl_yearTransfer_p[,yr], 
                                                (diff - fleets.ctrl[[flnm]]$LandObl_minimis_p[,yr] - 1),
                                                 fleets.ctrl[[flnm]]$LandObl_yearTransfer_p[,yr]))*Cr.f[,i]
-                                                
-              cat(‘CATCH after Landing Obligation Exemptions: ', catch_Elo[sts]/Cr.f[sts,i], '\n') ## testing
+              
+              cat("CATCH after Landing Obligation Exemptions", catch_Elo[sts]/Cr.f[sts,i], "\n") 
+              
               # update ret.m to account for the discards due to minimise exemption.
               for(st in sts){
               #if(st == 'OTH')
